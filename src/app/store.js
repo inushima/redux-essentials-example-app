89bf29ace9494f2f96bfdddc11ded90a735caaa6
inushima <t.inushima@gmail.com>
2021-04-30 10:45:10 +0900

    complete

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import postReducer from '../features/posts/postsSlice'
 
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    posts: postReducer
+  },
 })
